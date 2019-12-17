3b081d7b133a4188e719735ed0ecbd30528ac728
patra12 <Patra@kussoft.net>
2019-12-17 20:05:06 +0530

    category api (CRUD) is compleated

--- server/controller/category.js
+++ server/controller/category.js
@@ -1,139 +1,113 @@
 var pool = require("../db")
 var tableName = 'category';
+var folderLoc = "/upload/categories/"
+
+
+
 module.exports = {
+	//get any value form catagory table
 	// get all Record from a table
 	getCategory: (req, res) => {
-		//creating connection
-		pool.getConnection((err, con) => {
+
+		// query
+		selectQuery = "SELECT * FROM " + tableName;
+		pool.query(selectQuery, (err, row) => {
 			if (!err) {
-				// query
-				selectQuery = "SELECT * FROM " + tableName;
-				con.query(selectQuery, (err, row) => {
-					// When done with the connection, release it.
-					con.release()
-					if (!err) {
-						//send data to frontend
-						res.send(row);
-					}
-					else {
-						//log query error message to server and stop execution
-						console.log(' Query Error', err)
-						res.end();
-					}
-				});
+				//send data to frontend
+				res.send(row);
 			}
 			else {
-				//log db error message to server and stop execution
-				console.log(" Db Error", err);
+				//log query error message to server and stop execution
+				console.log(' Query Error', err)
 				res.end();
 			}
-		})
+		});
 	},
 
 	// get single Record from table
 	monoCategory: (req, res) => {
-		//creating connection
-		pool.getConnection((err, con) => {
+		// query
+		selectQuery = "SELECT * FROM " + tableName + " where `categoryId`= ?";
+		pool.query(selectQuery, [req.params.id], (err, row) => {
 			if (!err) {
-				// query
-				selectQuery = "SELECT * FROM " + tableName + " where `categoryId`= ?";
-				con.query(selectQuery, [req.params.id], (err, row) => {
-					// When done with the connection, release it.
-					con.release()
-					if (!err) {
-						//send data to frontend
-						res.send(row);
-					}
-					else {
-						//log query error message to server and stop execution
-						console.log('getCategory Query Error', err)
-						res.end();
-					}
-				});
+				//send data to frontend
+				res.send(row);
 			}
 			else {
-				//log db error message to server and stop execution
-				console.log("getCategory Db Error", err);
+				//log query error message to server and stop execution
+				console.log('getCategory Query Error', err)
 				res.end();
 			}
-		})
+		});
+
 	},
 
 	// insert Record into table
 	addCategory: (req, res) => {
-		//creating connection
-		pool.getConnection((err, con) => {
-			if (!err) {
-				parentId = ``
-				categoryname = req.body.firstName;
-				description = req.body.lastNname;
-				metatitle = req.body.password;
-				metakeywords = req.body.email;
-				metadescription = req.body.phone;
-				seourl = req.body.phone;
-				displayorder = req.body.phone;
-				categoryimage = req.body.phone;
-				bannerimageloc = req.body.phone;
-				status = req.body.status;
-				// query
-				inserQuery = "INSERT INTO " + tableName + "(`first_name`, `last_name`, `password`, `email`, `phone`, `status`";
-				inserQuery += ")VALUES(";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?)";
-				con.query(inserQuery, [first_name, last_name, password, email, phone, status], (err, row) => {
-					// When done with the connection, release it.
-					con.release()
-					if (!err) {
-						//send data to frontend
-						res.send("Date is inserted");
-					}
-					else {
-						//log query error message to server and stop execution
-						console.log("addCategory Query Error", err);
-						res.end();
-					}
-				})
-			}
-			else {
-				//log db error message to server and stop execution
-				console.log("addCategory DB ERROR", err);
-				res.end();
-			}
+		parentId = req.body.parentId;
+		categoryname = req.body.categoryName;
+		description = req.body.description;
+		metatitle = req.body.metaTitle;
+		metakeywords = req.body.metaKeywords;
+		metadescription = req.body.metaDescription;
+		seourl = req.body.seoUrl;
+		displayorder = req.body.displayOrder;
+		categoryimage = req.file.originalname;
+		bannerimageloc = req.body.bannerimageloc;
+		status = req.body.status;
+		console.log(categoryimage);
+		// res.send(categoryimage);
+		// query
+		inserQuery = "INSERT INTO " + tableName + "(`parentId`, `categoryname`,";
+		inserQuery += "`description`, `metatitle`, `metakeywords`, `metadescription`, ";
+		inserQuery += "`seourl`, `displayorder`,`categoryimage`, `bannerimageloc`, `status`)";
+		inserQuery += "VALUES( ?,?,?,?,?,?,?,?,?,?,?)";
+		pool.query(inserQuery, [parentId, categoryname, description, metatitle,
+			metakeywords, metadescription, seourl, displayorder, categoryimage, bannerimageloc, status], (err, row) => {
+				if (!err) {
+					//send data to frontend
+					res.send("Date is inserted");
+				}
+				else {
+					//log query error message to server and stop execution
+					console.log("addCategory Query Error", err);
+					res.end();
+				}
+			})
 
-		})
 	},
+
 
 	// delete Record from table
 	delCategory: (req, res) => {
-		//creating connection
-		pool.getConnection((err, con) => {
+
+
+		// var log = function getCol(col = String, table = String, idcol = String, id) {
+		// 	selectQuery = "SELECT " + col + " FROM `" + table + "` WHERE `" + idcol + "`=" + id;
+		// 	pool.query(selectQuery, (err, row) => {
+		// 		if (!err) {
+		// 			// return row
+		// 			return row[0].categoryimage;
+		// 		}
+		// 	});
+		// }
+		// // console.log(req.params.id);
+		// console.log(log);
+		// res.end();
+
+		// query
+		deleteQuery = "DELETE FROM " + tableName + " WHERE `categoryId` = " + req.params.id;
+		pool.query(deleteQuery, (err, row) => {
 			if (!err) {
-				// query
-				deleteQuery = "DELETE FROM " + tableName + " WHERE `categoryId` = " + req.params.id;
-				con.query(deleteQuery, (err, row) => {
-					// When done with the connection, release it.
-					con.release()
-					if (!err) {
-						//send data to frontend
-						res.send("Data deleted successfully");
-					}
-					else {
-						//log query error message to server and stop execution
-						console.log("delCategory Query Error ", err);
-						res.end();
-					}
-				})
+				//send data to frontend
+				res.send("Data deleted successfully");
 			}
 			else {
-				//log db error message to server and stop execution
-				console.log("delCategory DB ERROR", err);
+				//log query error message to server and stop execution
+				console.log("delCategory Query Error ", err);
 				res.end();
 			}
-		});
+		})
 	},
 
 	// update a Record in table
@@ -141,35 +115,45 @@ module.exports = {
 		//creating connection
 		pool.getConnection((err, con) => {
 			if (!err) {
-				first_name = req.body.firstName;
-				last_name = req.body.lastNname;
-				password = req.body.password;
-				email = req.body.email;
-				phone = req.body.phone;
+				parentId = req.body.parentId;
+				categoryname = req.body.categoryName;
+				description = req.body.description;
+				metatitle = req.body.metaTitle;
+				metakeywords = req.body.metaKeywords;
+				metadescription = req.body.metaDescription;
+				seourl = req.body.seoUrl;
+				displayorder = req.body.displayOrder;
+				categoryimage = req.file.originalname;
+				bannerimageloc = req.body.bannerimageloc;
 				status = req.body.status;
-				id = req.params.id;
+				id = req.params.id
 				// query
 				updateQuery = "UPDATE " + tableName + " SET";
-				updateQuery += "`first_name`= ?,";
-				updateQuery += "`last_name`= ?,";
-				updateQuery += "`password`= ?,";
-				updateQuery += "`email`= ?,";
-				updateQuery += "`phone`= ?,";
+				updateQuery += "`categoryname`= ?,";
+				updateQuery += "`description`= ?,";
+				updateQuery += "`metatitle`= ?,";
+				updateQuery += "`metakeywords`= ?,";
+				updateQuery += "`metadescription`= ?,";
+				updateQuery += "`seourl`= ?, ";
+				updateQuery += "`displayorder`= ?, ";
+				updateQuery += "`categoryimage`= ?, ";
+				updateQuery += "`bannerimageloc`= ?, ";
 				updateQuery += "`status`= ? ";
 				updateQuery += "WHERE `categoryId` = ?";
-				con.query(updateQuery, [first_name, last_name, password, email, phone, status, id], (err, row) => {
-					// When done with the connection, release it.
-					con.release()
-					if (!err) {
-						//send data to frontend
-						res.send("Data Updated successfully");
-					}
-					else {
-						//log query error message to server and stop execution
-						console.log("putCategory Query Error ", err);
-						res.end();
-					}
-				})
+				con.query(updateQuery, [categoryname, description, metatitle, metakeywords,
+					metadescription, seourl, displayorder, categoryimage, bannerimageloc, status, id], (err, row) => {
+						// When done with the connection, release it.
+						con.release()
+						if (!err) {
+							//send data to frontend
+							res.send("Data Updated successfully");
+						}
+						else {
+							//log query error message to server and stop execution
+							console.log("putCategory Query Error ", err);
+							res.end();
+						}
+					})
 			}
 			else {
 				//log db error message to server and stop execution
--- server/route/category.js
+++ server/route/category.js
@@ -1,13 +1,24 @@
 const app = require('express').Router();
+var multer = require("multer");
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'upload/categories/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now())
+    }
+})
+
+var upload = multer({ storage: storage });
 // Availing All Required Methods From  Controller For Route
 const category = require('../controller/category');
 
 // All Routes for API
 app.get('/getcategory', category.getCategory);
 app.get('/monocategory/:id', category.monoCategory);
-app.post('/addcategory', category.addCategory);
-app.put('/putcategory/:id', category.putCategory);
+app.post('/addcategory', upload.single('categoryimage'), category.addCategory);
+app.put('/putcategory/:id', upload.single('categoryimage'), category.putCategory);
 app.delete('/delcategory/:id', category.delCategory);
 
 module.exports = app;
