efdddc653a3e4c5f95eff7e0b03fb1553eee0488
patra12 <Patra@kussoft.net>
2019-12-24 17:52:57 +0530

    minor

--- server/route/product.js
+++ server/route/product.js
@@ -1,21 +1,14 @@
 const app = require('express').Router();
 var multer = require("multer");
 
-const fileUpload = require('express-fileupload');
-// default options
-app.use(fileUpload());
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'upload/products/')
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname);
+        cb(null, file.originalname + '-' + Date.now())
     }
-});
-
-// var up = multer({
-//     dest: 'upload/products/'
-// });
+})
 
 var upload = multer({ storage: storage });
 // Availing All Required Methods From  Controller For Route
@@ -24,10 +17,10 @@ const product = require('../controller/product');
 // All Routes for API
 app.get('/getproduct', product.getProduct);
 app.get('/monoproduct/:id', product.monoProduct);
-app.post('/addproduct', upload.arry('productImage', 5), product.addProduct);
+app.post('/addproduct', upload.array('productImage', 5), product.addProduct);
 app.put('/putproduct/:id', product.putProduct);
 app.delete('/delproduct/:id', product.delProduct);
 
-app.post('/test', product.delProduct);
+// app.post('/test', product.callx);
 
 module.exports = app;
