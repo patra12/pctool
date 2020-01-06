e7971aea3b75d89443c21f756edc034afdee57fb
patra12 <Patra@kussoft.net>
2020-01-06 19:03:03 +0530

    vutify enabled during build mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ module.exports = {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     // '@nuxtjs/eslint-module',
-    // '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify'
   ],
   /*
    ** Nuxt.js modules
