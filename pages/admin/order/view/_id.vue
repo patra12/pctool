56572c2d0d93bf4498bacdbc5c5132d2f8f6bdf9
patra12 <Patra@kussoft.net>
2020-01-10 13:43:09 +0530

    view full detailse of orders  this page is made

---
+++ pages/admin/order/view/_id.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-container class="bv-example-row list pa-0">
+    <v-row class="bg">
+      <h4 class="font-weight-light">Detailse view</h4>
+    </v-row>
+    <v-row class="bg-content text-center">
+      <!-- <div class="row w-100 ma-0">
+        <v-col class="border">
+          <b>#</b>
+        </v-col>
+        <v-col class="border">
+          <b>Orders</b>
+        </v-col>
+        <v-col class="border">
+          <b>Alias</b>
+        </v-col>
+        <v-col class="border">
+          <b>Status</b>
+        </v-col>
+        <v-col class="border">
+          <b>Action</b>
+        </v-col>
+      </div>-->
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>User Name</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Phone</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Email</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Order Id</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Order Date</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Order Status</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Order amount</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Quentity</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Shipping Cost</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Packging Fee</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Packging Fee</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Discount</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Grand Total</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+      <div class="row w-100 ma-0">
+        <v-col sm="3" class="border font-weight-light">
+          <b>Payment Status</b>
+        </v-col>
+        <v-col sm="9" class="border font-weight-light text-left">abc</v-col>
+      </div>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "OrderDetailse",
+  layout: "admin/defaultAdmin"
+};
+</script>
