81bcc55740cc2243a0f4486924fdb03b2edab643
patra12 <Patra@kussoft.net>
2020-01-13 17:57:50 +0530

    clean code and remove dependency

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,144 +1,95 @@
-var pool = require("../db")
+var pool = require("../db/db")
 
 module.exports = {
-	getUser: (req, res) => {
-		pool.getConnection((err, con) => {
-			if (!err) {
-				selectQuery = "SELECT * FROM `user`";
-				con.query(selectQuery, (err, row) => {
-					con.release();
-					if (!err) {
-						res.send(row);
-					}
-					else {
-						console.log('getUser Query Error', err)
-						res.end();
-					}
-				});
-			}
-			else {
-				console.log("getUser Db Error", err);
+	async getUser(req, res) {
+		selectQuery = "SELECT * FROM `user`";
+		pool.query(selectQuery)
+			.then(row => {
+				res.send(row);
+			})
+			.catch(err => {
+				console.log('getUser Query Error', err)
 				res.end();
-			}
-		})
+			});
 	},
 
-	monoUser: (req, res) => {
-		pool.getConnection((err, con) => {
-			if (!err) {
-				selectQuery = "SELECT * FROM `user` where `userId`= ?";
-				con.query(selectQuery, [req.params.id], (err, row) => {
-					con.release()
-					if (!err) {
-						res.send(row);
-					}
-					else {
-						console.log('getUser Query Error', err)
-						res.end();
-					}
-				});
-			}
-			else {
-				console.log("getUser Db Error", err);
+	async monoUser(req, res) {
+		selectQuery = "SELECT * FROM `user` where `userId`= ?";
+		con.query(selectQuery, [req.params.id])
+			.then(row => {
+				res.send(row);
+			})
+			.catch(err => {
+				console.log('getUser Query Error', err)
 				res.end();
-			}
-		})
+			});
 	},
 
-	addUser: (req, res) => {
-		pool.getConnection((err, con) => {
-			if (!err) {
-				first_name = req.body.firstName;
-				last_name = req.body.lastNname;
-				password = req.body.password;
-				email = req.body.email;
-				phone = req.body.phone;
-				status = req.body.status;
+	async addUser(req, res) {
 
-				inserQuery = "INSERT INTO `user`(`first_name`, `last_name`, `password`, `email`, `phone`, `status`";
-				inserQuery += ")VALUES(";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?,";
-				inserQuery += " ?)";
-				con.query(inserQuery, [first_name, last_name, password, email, phone, status], (err, row) => {
-					con.release()
-					if (!err) {
-						res.send("Date is inserted");
-					}
-					else {
-						console.log("addUser Query Error", err);
-						res.end();
-					}
-				})
-			}
-			else {
-				console.log("addUser DB ERROR", err);
+		first_name = req.body.firstName;
+		last_name = req.body.lastNname;
+		password = req.body.password;
+		email = req.body.email;
+		phone = req.body.phone;
+		status = req.body.status;
+
+		inserQuery = "INSERT INTO `user`(`first_name`, `last_name`, `password`, `email`, `phone`, `status`";
+		inserQuery += ")VALUES(";
+		inserQuery += " ?,";
+		inserQuery += " ?,";
+		inserQuery += " ?,";
+		inserQuery += " ?,";
+		inserQuery += " ?,";
+		inserQuery += " ?)";
+		con.query(inserQuery, [first_name, last_name, password, email, phone, status])
+			.then(row => {
+				res.send("Date is inserted");
+			})
+			.catch(err => {
+				console.log("addUser Query Error", err);
 				res.end();
-			}
-
-		})
+			});
 	},
-	delUser: (req, res) => {
-		pool.getConnection((err, con) => {
-			if (!err) {
-				deleteQuery = "DELETE FROM `user` WHERE userId = " + req.params.id;
-				console.log(deleteQuery);
-				con.query(deleteQuery, (err, row) => {
-					con.release()
-					if (!err) {
-						res.send("Data deleted successfully");
-						console.log();
-					}
-					else {
-						console.log("delUser Query Error ", err);
-						res.end();
-					}
-				})
-			}
-			else {
-				console.log("delUser DB ERROR", err);
-				res.end();
-			}
-		});
-	},
-	putUsre: (req, res) => {
-		pool.getConnection((err, con) => {
-			if (!err) {
-				first_name = req.body.firstName;
-				last_name = req.body.lastNname;
-				password = req.body.password;
-				email = req.body.email;
-				phone = req.body.phone;
-				status = req.body.status;
-				id = req.params.id;
+	async delUser(req, res) {
 
-				updateQuery = "UPDATE `user` SET";
-				updateQuery += "`first_name`= ?,";
-				updateQuery += "`last_name`= ?,";
-				updateQuery += "`password`= ?,";
-				updateQuery += "`email`= ?,";
-				updateQuery += "`phone`= ?,";
-				updateQuery += "`status`= ? ";
-				updateQuery += "WHERE `userId` = ?";
-				con.query(updateQuery, [first_name, last_name, password, email, phone, status, id], (err, row) => {
-					con.release()
-					if (!err) {
-						res.send("Data Updated successfully");
-					}
-					else {
-						console.log("putUser Query Error ", err);
-						res.end();
-					}
-				})
-			}
-			else {
-				console.log("putUser DB ERROR", err);
+		deleteQuery = "DELETE FROM `user` WHERE userId = " + req.params.id;
+		console.log(deleteQuery);
+		con.query(deleteQuery)
+			.then(row => {
+				res.send("Data deleted");
+			})
+			.catch(err => {
+				console.log("delUser Query Error ", err);
 				res.end();
-			}
-		});
+			});
+	},
+
+	async putUsre(req, res) {
+		first_name = req.body.firstName;
+		last_name = req.body.lastNname;
+		password = req.body.password;
+		email = req.body.email;
+		phone = req.body.phone;
+		status = req.body.status;
+		id = req.params.id;
+
+		updateQuery = "UPDATE `user` SET";
+		updateQuery += "`first_name`= ?,";
+		updateQuery += "`last_name`= ?,";
+		updateQuery += "`password`= ?,";
+		updateQuery += "`email`= ?,";
+		updateQuery += "`phone`= ?,";
+		updateQuery += "`status`= ? ";
+		updateQuery += "WHERE `userId` = ?";
+		con.query(updateQuery, [first_name, last_name, password, email, phone, status, id])
+			.then(row => {
+				res.send("Data Updated");
+			})
+			.catch(err => {
+				console.log("putUser Query Error ", err);
+				res.end();
+			})
 	}
 
 }
