ec94c55d41bfe020c8e6729340aaf5a68d165a87
patra12 <Patra@kussoft.net>
2020-01-14 12:45:48 +0530

    left side category pannel is now dynamic

--- components/front/category.vue
+++ components/front/category.vue
@@ -1,37 +1,15 @@
 <template>
-  <div class="col-md-3">
-    <div class="list-group">
-      <p>Construction</p>
+  <div class="col-md-3 pb-5">
+    <div v-for="(parent,index) in left_pannel_parent_categories" :key="index" class="list-group">
+      <p>{{parent.name}}</p>
       <hr class="hrMod" />
-      <a href="#" class="list-group-item">
-        <span>Wall Saws</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Ring Saws</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Pro Slab Saws</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Dry Blades</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Asphalt Blades</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Grinding wheels</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Core Bits</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Early entry blades</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>AOC - Asphalt ove concrete</span>
-      </a>
-      <a href="#" class="list-group-item">
-        <span>Flooring</span>
+      <a
+        v-for="(category,index) in parent.categories"
+        :key="index"
+        :href="index"
+        class="list-group-item"
+      >
+        <span>{{category.name}}</span>
       </a>
     </div>
   </div>
@@ -39,6 +17,73 @@
 
 <script>
 export default {
-  name: "category"
+  name: "category",
+  data() {
+    return {
+      left_pannel_parent_categories: [
+        {
+          id: 1,
+          name: "Construction :",
+          categories: [
+            {
+              id: 1,
+              name: "Wall Saws"
+            },
+            {
+              id: 2,
+              name: "Ring Saws"
+            },
+            {
+              id: 3,
+              name: "Pro Slab Saws"
+            },
+            {
+              id: 4,
+              name: "Dry Blades"
+            },
+            {
+              id: 5,
+              name: "Asphalt Blades"
+            },
+            {
+              id: 6,
+              name: "Grinding wheels"
+            },
+            {
+              id: 7,
+              name: "Core Bits"
+            },
+            {
+              id: 8,
+              name: "Early entry blades"
+            },
+            {
+              id: 8,
+              name: "AOC - Asphalt ove concrete"
+            },
+            {
+              id: 8,
+              name: "Flooring"
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "Line of equipement :",
+          categories: [
+            {
+              id: 1,
+              name: "Dynatech - PCD Head System"
+            },
+            {
+              id: 1,
+              name: "Vacuworx"
+            }
+          ]
+        }
+      ]
+    };
+  },
+  methods() {}
 };
 </script>
