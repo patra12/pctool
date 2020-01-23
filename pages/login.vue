3813aa3cb82025443cb89cf538bf1a6a081dd3aa
patra12 <patra@kussoft.net>
2020-01-23 17:14:58 +0530

    login page design

---
+++ pages/login.vue
@@ -0,0 +1,70 @@
+<template>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-12 mx-auto">
+        <div class="card">
+            <article class="card-body">
+                <a href="" class="float-right btn btnSignup">Sign up</a>
+                <p class="card-title">Sign in</p>
+                <hr>
+                <form>
+                    <div class="form-group">
+                        <input name="" class="form-control" placeholder="Email or login" type="email">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="******" type="password">
+                    </div>                 
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a class="small" href="#">Forgot password?</a>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <button type="submit" class="btn btnLogin btn-block">Login</button>
+                            </div>
+                        </div>
+                    </div>                                                             
+                </form>
+            </article>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.card{
+    -webkit-box-shadow: 0px 0px 22px -10px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 0px 22px -10px rgba(0,0,0,0.75);
+    box-shadow: 0px 0px 22px -10px rgba(0,0,0,0.75);
+    margin: 30px 0;
+}
+.card-title{
+    font-size: 20px;
+}
+.btnLogin{
+    background: #d23940;
+    border: #d23940;
+    color: #fff;
+}
+.btnLogin:hover{
+    background: #ac131b;
+    border: #ac131b;
+    color: #fff;
+}
+.btnSignup{
+    border: 1px solid #d23940 !important;
+    color: #d23940 !important;
+}
+.btnSignup:hover{
+    background:#d23940;
+    border: #d23940;
+    color: #fff !important;
+}
+</style>
