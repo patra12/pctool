d5f4bde435bb759c3cbf0402641710a206cf4107
patra12 <patra@kussoft.net>
2020-02-03 17:04:41 +0530

    add user accoun page

---
+++ pages/user/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    user account
+    <button>wish list</button>
+    <button>logout</button>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+</style>
