ebbcee6ea981d7021386c4a1bc54237119ddbfaf
patra12 <patra@kussoft.net>
2020-02-06 12:18:52 +0530

    extra comented code removed

--- pages/admin/pages/_id.vue
+++ pages/admin/pages/_id.vue
@@ -3,41 +3,92 @@
     <v-row class="form pt-0">
       <v-col class="pt-0">
         <v-row>
-          <v-col outlined class="head">
+          <v-col
+            outlined
+            class="head"
+          >
             <h4 class="font-weight-light text-light">Edit Page</h4>
             <nuxt-link to="/admin/pages">
-              <v-icon title="back" class="right" color="white darken-1">mdi-arrow-left-bold</v-icon>
+              <v-icon
+                title="back"
+                class="right"
+                color="white darken-1"
+              >mdi-arrow-left-bold</v-icon>
             </nuxt-link>
           </v-col>
         </v-row>
         <v-row>
           <v-col class="bg-content">
-            <v-form ref="form" v-model="valid" lazy-validation>
-              <v-text-field v-model="title" label="Title"></v-text-field>
+            <v-form
+              ref="form"
+              v-model="valid"
+              lazy-validation
+            >
+              <v-text-field
+                v-model="title"
+                label="Title"
+              ></v-text-field>
 
-              <v-text-field v-model="menuTitle" label="Menu Title"></v-text-field>
+              <v-text-field
+                v-model="menuTitle"
+                label="Menu Title"
+              ></v-text-field>
 
-              <v-select :items="showTitleItems" v-model="showTitle" label="Show Title"></v-select>
+              <v-select
+                :items="showTitleItems"
+                v-model="showTitle"
+                label="Show Title"
+              ></v-select>
 
-              <v-text-field v-model="seoUrl" label="seo URL"></v-text-field>
+              <v-text-field
+                v-model="seoUrl"
+                label="seo URL"
+              ></v-text-field>
 
               <p class="grey--text text--darken-1 pt-3 mb-0">Page Description</p>
 
-              <div v-model="pageDescription" v-quill:page class="quill-editor"></div>
+              <div
+                v-model="pageDescription"
+                v-quill:page
+                class="quill-editor"
+              ></div>
 
-              <v-text-field v-model="metaTitle" label="Meta title"></v-text-field>
+              <v-text-field
+                v-model="metaTitle"
+                label="Meta title"
+              ></v-text-field>
 
-              <v-text-field v-model="metaKeyword" label="Meta keyword"></v-text-field>
+              <v-text-field
+                v-model="metaKeyword"
+                label="Meta keyword"
+              ></v-text-field>
 
               <p class="grey--text text--darken-1 pt-3 mb-0">Meta Description</p>
 
-              <div v-model="metaDescription" v-quill:meta class="quill-editor"></div>
+              <div
+                v-model="metaDescription"
+                v-quill:meta
+                class="quill-editor"
+              ></div>
 
-              <v-text-field v-model="displayOrder" type="number" label="Display Order"></v-text-field>
+              <v-text-field
+                v-model="displayOrder"
+                type="number"
+                label="Display Order"
+              ></v-text-field>
 
-              <v-select :items="statusItems" v-model="status" label="Status"></v-select>
+              <v-select
+                :items="statusItems"
+                v-model="status"
+                label="Status"
+              ></v-select>
 
-              <v-btn @click="putData()" class="my-5 float-right" large color="primary">Update</v-btn>
+              <v-btn
+                @click="putData()"
+                class="my-5 float-right"
+                large
+                color="primary"
+              >Update</v-btn>
             </v-form>
           </v-col>
         </v-row>
@@ -87,21 +138,22 @@ export default {
     ...mapGetters({ findPagex: "admin/page/findPage" })
   },
   methods: {
-    getStatus() {
+    getStatus () {
       return this.status === "Active" ? "Y" : "N";
     },
-    setStatus(status) {
+    setStatus (status) {
       var change_status = status === "Y" ? "Active" : "Not Active";
       this.status = change_status;
     },
-    getShowTitle() {
+    getShowTitle () {
       return this.showTitle === "Yes" ? "Y" : "N";
     },
-    setShowTitle(status) {
+    setShowTitle (status) {
       var changed_status = status === "Y" ? "Yes" : "No";
       this.showTitle = changed_status;
     },
-    putData() {
+    //for edit data in page using state
+    putData () {
       const form = {
         title: this.title,
         menuTitle: this.menuTitle,
@@ -132,26 +184,10 @@ export default {
       });
 
       this.$router.push("/admin/pages");
-      // this.$axios({
-      //   url: "/putpage/" + this.id,
-      //   method: "PUT",
-      //   headers: {
-      //     header: { "Content-Type": "application/x-www-form-urlencoded" }
-      //   },
-      //   data: form
-      // })
-      //   .then(res => {
-      //     this.$router.push("/admin/pages");
-      //     // console.log(res.data);
-      //   })
-      //   .catch(error => {
-      //     // handle error
-      //     console.log(error);
-      //   });
     },
 
     //get single page data from state
-    singlePage() {
+    singlePage () {
       let id = this.$route.params.id;
       let pageData = this.findPagex(id);
 
@@ -170,10 +206,10 @@ export default {
   },
 
   //for loading data during page reloding
-  async fetch({ store }) {
+  async fetch ({ store }) {
     await store.dispatch("admin/page/getPageData");
   },
-  mounted() {
+  mounted () {
     this.singlePage();
   }
 };
