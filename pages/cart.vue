580edb0242aad0e54363c13f14e22dc28c87cf82
birja <birja@kusmail.com>
2020-02-10 15:45:44 +0530

    remove comment and chnage checkout to signin url

--- pages/cart.vue
+++ pages/cart.vue
@@ -83,71 +83,6 @@
             </div>
           </div>
           <hr />
-          <!-- END PRODUCT -->
-          <!-- PRODUCT -->
-          <!-- <div class="row">
-            <div class="col-12 col-sm-12 col-md-2 text-center">
-              <img
-                class="img-responsive"
-                src="http://placehold.it/120x80"
-                alt="prewiew"
-                width="120"
-                height="80"
-              />
-            </div>
-            <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6 productInfo">
-              <h4 class="product-name">
-                <strong>Product Name</strong>
-              </h4>
-              <h4>
-                <small>Product description</small>
-              </h4>
-            </div>
-            <div class="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">
-              <div class="col-3 col-sm-3 col-md-6 text-right exactPrice">
-                <h6>
-                  <strong>
-                    25.00
-                    <span class="text-muted">x</span>
-                  </strong>
-                </h6>
-              </div>
-              <div class="col-4 col-sm-4 col-md-4">
-                <div class="quantity">
-                  <input
-                    type="button"
-                    value="+"
-                    class="plus"
-                  />
-                  <input
-                    type="number"
-                    step="1"
-                    max="99"
-                    min="1"
-                    value="1"
-                    title="Qty"
-                    class="qty"
-                    size="4"
-                  />
-                  <input
-                    type="button"
-                    value="-"
-                    class="minus"
-                  />
-                </div>
-              </div>
-              <div class="col-2 col-sm-2 col-md-2 text-right">
-                <button
-                  type="button"
-                  class="btn btn-outline-danger btn-xs"
-                >
-                  <i class="mdi mdi-delete"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-          <hr /> -->
-          <!-- END PRODUCT -->
 
           <div class="row">
             <div class="col-md-6">
@@ -186,7 +121,7 @@
             style="margin: 10px"
           >
             <nuxt-link
-              to="/checkout"
+              to="/signin"
               class="btn btn-info pull-right btnBuy"
             >Checkout</nuxt-link>
             <div
