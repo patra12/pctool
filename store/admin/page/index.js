8c30aa2af7b31088e5f03d034260670750f478b8
patra12 <patra@kussoft.net>
2020-02-28 16:55:24 +0530

    code clean

--- store/admin/page/index.js
+++ store/admin/page/index.js
@@ -5,17 +5,17 @@ export const state = () => ({
 
 export const mutations = {
     //setting all data for listing at first time
-    setPageData(state, pageData) {
+    setPageData (state, pageData) {
         state.pages = pageData
     },
-    delApage(state, delId) {
+    delApage (state, delId) {
         const index_of_object_array = state.pages.findIndex(index => index.pageId == delId);
         state.pages.splice(index_of_object_array, 1);
     },
-    addpage(state, page) {
+    addpage (state, page) {
         state.pages.push(page);
     },
-    editpage(state, page) {
+    editpage (state, page) {
         const index_of_object_array = state.pages.findIndex(index => index.pageId == page.pageId);
         Vue.set(state.pages, index_of_object_array, page)
     }
@@ -35,21 +35,23 @@ export const getters = {
 //         console.log(err);
 //     })
 export const actions = {
-    async getPageData({ commit }) {
+    async getPageData ({ commit }) {
         const getdata = (await this.$axios.get("/getpage")).data;
         commit('setPageData', getdata);
     },
-    async delpage({ commit }, id) {
+    async delpage ({ commit }, id) {
         const x = await this.$axios.delete("/delpage/" + id);
         commit('delApage', id);
         return 1;
     },
-    async addpage({ commit }, page) {
+    async addpage ({ commit }, page) {
         const x = (await this.$axios.post("/addpage", page)).data;
+        console.log("x==>", x);
+        console.log("page==>", x);
         commit("addpage", x);
         return true;
     },
-    async editpage({ commit }, id) {
+    async editpage ({ commit }, id) {
         const y = (await this.$axios.put("/putpage/" + id.id, id.data)).data;
         commit('editpage', y);
     }
