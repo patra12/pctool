da1a9f46312a5f590e2d18823bb51fa08661e178
patra12 <patra@kussoft.net>
2020-02-28 16:54:45 +0530

    api modified

--- server/controller/tempCart.js
+++ server/controller/tempCart.js
@@ -2,7 +2,7 @@ var pool = require("../db/db");
 
 module.exports = {
     // insert product id and session id in temp_order table
-    addProductId (req, res) {
+    async addProductId (req, res) {
         const sessionid = req.body.sessionid
         const productId = req.body.productId
         const price = req.body.price
@@ -13,25 +13,29 @@ module.exports = {
         insertQuery += ' productId,price,qty) VALUES ';
         insertQuery += "(?,?,?,?)";
 
-        pool.query(insertQuery, [sessionid, productId, price,quantity])
+        lastRowId = await pool.query(insertQuery, [sessionid, productId, price, quantity])
             .then(row => {
-                //send data to frontend
-                res.send("Data is inserted");
+                return row.insertId;
             })
             .catch(err => {
-                //log query error message to server and stop execution
-                console.log("Query Error", err);
-                res.end();
+                return ("Query Error")
             });
+
+        getrow = "select * from temp_order where tempId = ?";
+        lastRow = await pool.query(getrow, [lastRowId]).then(res => {
+            return res[0];
+        })
+        res.json(lastRow);
     },
+
     // for count total and show on add to cart button
     async gettotaldata (req, res) {
 
-       let selectQuery = " SELECT count(sessionid) as total, ";
-       selectQuery += "productId, price, sellprice ,size, qty,";
-       selectQuery +=" date, freeship from temp_order where sessionid = ?";
+        let selectQuery = " SELECT count(sessionid) as total, ";
+        selectQuery += "productId, price, sellprice ,size, qty,";
+        selectQuery += " date, freeship from temp_order where sessionid = ?";
 
-        pool.query(selectQuery,[req.params.id])
+        pool.query(selectQuery, [req.params.id])
             .then(row => {
                 res.send(row);
             })
@@ -47,7 +51,7 @@ module.exports = {
         selectQuery += "temp_order AS T ON P.productId=T.productId";
         selectQuery += " where T.sessionid= ?";
 
-        pool.query(selectQuery,[req.params.id])
+        pool.query(selectQuery, [req.params.id])
             .then(row => {
                 res.send(row);
             })
@@ -60,7 +64,7 @@ module.exports = {
     async delData (req, res) {
 
         deleteQuery = "DELETE FROM temp_order WHERE tempId = ?";
-        pool.query(deleteQuery,[req.params.id])
+        pool.query(deleteQuery, [req.params.id])
             .then(row => {
                 res.send("Data deleted");
             })
@@ -69,7 +73,7 @@ module.exports = {
                 res.end();
             });
     },
-    async updateQuantity(req,res){
+    async updateQuantity (req, res) {
 
         //DATA
         quantity = req.body.quantity;
@@ -77,23 +81,23 @@ module.exports = {
         proiductId = req.body.productId;
 
         //query string
-        updateQuery =  "UPDATE temp_order SET qty = ? ";
+        updateQuery = "UPDATE temp_order SET qty = ? ";
         updateQuery += "WHERE sessionid = ? and productId = ?";
 
         //Executing Query
-        pool.query(updateQuery,[quantity,sessionid,proiductId])
-        .then(row=>{
-            res.end('Quantity updated');
-        })
-        .catch(err=>{
-            console.log("update Query Error ",err);
-            res.status(500).send('Error');
-        });
+        pool.query(updateQuery, [quantity, sessionid, proiductId])
+            .then(row => {
+                res.end('Quantity updated');
+            })
+            .catch(err => {
+                console.log("update Query Error ", err);
+                res.status(500).send('Error');
+            });
     },
     async flushData (req, res) {
 
         deleteQuery = "DELETE FROM temp_order WHERE sessionid = ?";
-        pool.query(deleteQuery,[req.params.id])
+        pool.query(deleteQuery, [req.params.id])
             .then(row => {
                 res.send("cart is empty");
             })
