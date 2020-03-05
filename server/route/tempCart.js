357c69303a52ac5fe8f5a0021b9b11caece903d6
patra12 <patra@kussoft.net>
2020-03-05 19:05:59 +0530

    give proper name to routes for better identification

--- server/route/tempCart.js
+++ server/route/tempCart.js
@@ -2,12 +2,12 @@ const app = require('express').Router()
 
 const tempCart = require('../controller/tempCart');
 
-//Allfor temp order
+//All for temp order
 app.post('/addProductId', tempCart.addProductId);
-app.get('/gettotaldata/:id', tempCart.gettotaldata);
+app.get('/getTempDataBySession/:id', tempCart.getTempDataBySession);
 app.get('/getDataCartpage/:id', tempCart.getDataCartpage);
 app.delete('/delData/:id', tempCart.delData);
-app.put('/updatequantity',tempCart.updateQuantity);
+app.put('/updatequantity', tempCart.updateQuantity);
 
 app.delete('/flushData/:id', tempCart.flushData);
 
